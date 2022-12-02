372f8e5a574ed8e12189e24a2e1704f18a805d54
Filatov Dmitry <popalosh@gmail.com>
2022-12-02 22:14:22 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import TekkenApp from './components/TekkenApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TekkenApp
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <tekken-app></tekken-app>
+  </div>
+</template>
+
+<style>
+  @import '../node_modules/typeface-roboto/index.css'
+</style>
---
+++ src/components/TekkenApp.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+  name: 'TekkenApp',
+  props: {
+    msg: String
+  }
+}
+
+</script>
+
+<template>
+  <div class="navbar">
+    <div class="left">
+        <p class="roboto-font-white">Бронирование</p>
+    </div>
+    <div class="right">
+        <button class="btn roboto-btn">Регистрация</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.navbar {
+  background:  #393186;
+  padding: 16px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+
+.roboto-font-white {
+  font-family: Roboto;
+  font-size: 20px;
+  font-weight: 400;
+  color: white;
+}
+.roboto-btn {
+  font-family: Roboto;
+  font-size: 16px;
+  border: 1px solid black;
+  background-color: white;
+  color: black;
+  font-weight: 400;
+}
+
+.right {
+  float: right!important;
+}
+
+.left {
+  float: left!important;
+}
+
+</style>
